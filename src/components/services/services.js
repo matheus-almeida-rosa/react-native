c3e341d78b1e216381c950df7390039ad58a790f
Matheus de Almeida Rosa <matheusp@take.net>
2019-04-02 19:56:04 -0300

    fix api return

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -3,10 +3,10 @@ import { Card, ListItem, Avatar } from 'react-native-elements';
 import { withNavigation } from 'react-navigation';
 
 import Service from './service';
-import C2m2Client from '../../services/api/c2m2Client';
+import { getAttendanceList } from '../../services/api/c2m2Client';
 
-var client = new C2m2Client();
-const users = client.getAttendanceList();
+const services = getAttendanceList();
+console.log(getAttendanceList);
 
 class Services extends Component {
     render () {
@@ -14,9 +14,9 @@ class Services extends Component {
         return (
             <Card containerStyle={{ padding: 0, marginBottom: 20 }}>
             {
-                users.map((u, i) => {
+                services.map((u, i) => {
                     return (
-                        <Service onPress= { () => navigate('Installs', { name: u.name }) } index={i} title={u.name} avatar={u.avatar} description={u.description}></Service>
+                        <Service onPress= { () => navigate('Installs', { name: u.name }) } key={i} index={i} title={u.name} avatar={u.avatar} description={u.description}></Service>
                     );
                 })
             }
--- src/services/api/c2m2Client.js
+++ src/services/api/c2m2Client.js
@@ -2,59 +2,52 @@ import React from 'react';
 
 var url = 'hmg.c2m2.take.io/v2';
 
-export default class C2m2Client {
-    getAttendanceList() {
-        return 
-        [
-            {
-                name: 'Estou sem luz',
-                avatar: require('../../../assets/PowerOutage.png'),
-                description: 'Estou sem luz'
-            },
-            {
-                name: 'Meus débitos',
-                avatar: require('../../../assets/Debt.png'),
-                description: 'Meus débitos'
-            },
-            {
-                name: 'Mudança de endereço',
-                avatar: require('../../../assets/ChangeAddress.png'),
-                description: 'Mudança de endereço'
-            },
-            {
-                name: 'Histórico de atendimento',
-                avatar: require('../../../assets/AttendanceHistory.png'),
-                description: 'Histórico de atendimento'
-            },
-            {
-                name: 'Corte para conserto',
-                avatar: require('../../../assets/MaintenanceCutOff.png'),
-                description: 'Corte para conserto'
-            },
-            {
-                name: 'Segunda via',
-                avatar: require('../../../assets/DuplicateBill.png'),
-                description: 'Segunda via'
-            },
-            {
-                name: 'Conta por email',
-                avatar: require('../../../assets/BillByEmail.png'),
-                description: 'Conta por email'
-            },
-            {
-                name: 'Religa',
-                avatar: require('../../../assets/DisconnectorReplacement.png'),
-                description: 'Religa'
-            },
-            {
-                name: 'Mudança de endereço',
-                avatar: require('../../../assets/ChangeAddress.png'),
-                description: 'Mudança de endereço'
-            }
-        ];
-    }
-
-    getDuplicateBill() {
-        return [{ fileLocation: '../../../dowloads/bill.pdf' }];
-    }
-}
+export function getAttendanceList() {
+    return [
+        {
+            name: 'Estou sem luz',
+            avatar: require('../../../assets/PowerOutage.png'),
+            description: 'Estou sem luz'
+        },
+        {
+            name: 'Meus débitos',
+            avatar: require('../../../assets/Debt.png'),
+            description: 'Meus débitos'
+        },
+        {
+            name: 'Mudança de endereço',
+            avatar: require('../../../assets/ChangeAddress.png'),
+            description: 'Mudança de endereço'
+        },
+        {
+            name: 'Histórico de atendimento',
+            avatar: require('../../../assets/AttendanceHistory.png'),
+            description: 'Histórico de atendimento'
+        },
+        {
+            name: 'Corte para conserto',
+            avatar: require('../../../assets/MaintenanceCutOff.png'),
+            description: 'Corte para conserto'
+        },
+        {
+            name: 'Segunda via',
+            avatar: require('../../../assets/DuplicateBill.png'),
+            description: 'Segunda via'
+        },
+        {
+            name: 'Conta por email',
+            avatar: require('../../../assets/BillByEmail.png'),
+            description: 'Conta por email'
+        },
+        {
+            name: 'Religa',
+            avatar: require('../../../assets/DisconnectorReplacement.png'),
+            description: 'Religa'
+        },
+        {
+            name: 'Mudança de endereço',
+            avatar: require('../../../assets/ChangeAddress.png'),
+            description: 'Mudança de endereço'
+        }
+    ];
+};
